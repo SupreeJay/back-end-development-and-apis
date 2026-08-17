aaccd58c33a0a7748b6e3a276342d1cec52eedac
Oladoja Sirajudeen <145358384+SupreeJay@users.noreply.github.com>
2026-08-17 11:25:36 +0000

    The nodeJs prime number checker

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,19 @@
+function isPrime(number) {
+    if (typeof number !== 'number' || Number.isNaN(number) || number < 2) {
+        return false;
+    }
+    if (number === 2) return true;
+    if (number % 2 === 0) return false;
+
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+        if (number % i === 0) return false;
+    }
+
+    return true;
+}
+
+if (typeof module === 'undefined') {
+    globalThis.module = { exports: {} };
+}
+
+module.exports = { isPrime };
